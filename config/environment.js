f3928a57692640155624a16a4fbb523152a60f93
Brandon J McKay <themckaystudios@gmail.com>
2019-01-13 23:48:08 -0800

    move to pods structure

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'tricky-code',
+    podModulePrefix: 'tricky-code/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
